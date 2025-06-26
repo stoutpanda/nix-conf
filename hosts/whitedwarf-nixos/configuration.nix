eb1ddb7a962c0cacadf672dc5a91e0da99314c61
stoutpanda <stoutpanda@protonmail.com>
2025-06-25 20:24:07 -0500

    moving networking.

--- hosts/whitedwarf-nixos/configuration.nix
+++ hosts/whitedwarf-nixos/configuration.nix
@@ -16,6 +16,15 @@
   # LUKS encryption
   boot.initrd.luks.devices."luks-3e8010e8-8e35-45f0-8063-6f49fda6c09d".device = "/dev/disk/by-uuid/3e8010e8-8e35-45f0-8063-6f49fda6c09d";
 
+  # ASUS laptop control daemon
+  services.asusd = {
+    enable = true;
+    enableUserService = true;
+    asusdConfig = {
+      bat_charge_limit = 80;  # Set battery charge limit to 80%
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken.
--- hosts/whitedwarf-nixos/networking.nix
+++ hosts/whitedwarf-nixos/networking.nix
@@ -5,7 +5,11 @@
   
   # Enable NetworkManager
   networking.networkmanager.enable = true;
-  
+  # Enable the dhcp.  
+  networking.useDHCP = lib.mkDefault true;
+   networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
+
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
