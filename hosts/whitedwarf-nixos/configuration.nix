e234d9809109b59c6064fdadc30d6f8aebf589d0
stoutpanda <stoutpanda@protonmail.com>
2025-06-27 22:13:11 -0500

    needed hydenix + networking hostname

--- hosts/whitedwarf-nixos/configuration.nix
+++ hosts/whitedwarf-nixos/configuration.nix
@@ -8,6 +8,7 @@
 
   # Hostname configuration
   networking.hostName = "whitedwarf-nixos";
+  hydenix.hostname = "whitedwarf-nixos";
 
   # Bootloader configuration
   boot.loader.systemd-boot.enable = true;
